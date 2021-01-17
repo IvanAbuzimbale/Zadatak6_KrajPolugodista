2429bea9da30d81a43db38cb8e87bebe4aff0ca1
IvanAbuzimbale <58472137+IvanAbuzimbale@users.noreply.github.com>
2021-01-17 13:56:42 +0100

    Add files via upload

---
+++ Zadatak 6/Zadatak 6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadatak_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int zbroj = 0;
+            Console.Write("Unesite željeni broj n: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i <= n; i++)
+            {
+                zbroj = zbroj + i;
+            }
+            Console.WriteLine("Zbroj prvih n brojeva je " + zbroj);
+            Console.ReadKey();
+        }
+    }
+}
